eb06e01bd979094da727a02ebb6b1a4abad84cd0
ngf93 <nabiullinaguzel93@gmail.com>
2021-07-07 15:36:35 +0300

    unverified email

--- js/script.js
+++ js/script.js
@@ -26,20 +26,19 @@ $(".unverified_trigger").on('click', function() {
     $(".unverified_window").removeClass('d-none');
 });
 
-
-
-function toggleDiv(elem) {
-    // открываем/закрываем окно, добаляя/удаляя активный класс
-    elem.classList.toggle('d-none');
+// открываем/закрываем блок, добаляя/удаляя класс 'd-none'
+function closeDiv(elem) {
+    elem.classList.add('d-none');
 }
 //скрываем опции при клике вне селекта
-let item = document.getElementById('unverified_window');
+let item = document.querySelector('.unverified_window');
+let btn = document.querySelector('.unverified_trigger');
 document.addEventListener('click', function(e) {
     const target = e.target;
+    const currentBtn = target == btn || btn.contains(target);
     const currentDiv = target == item || item.contains(target);
-    const div_is_closed = options.classList.contains('d-none');
-    if (!currentDiv && !div_is_closed) {
-        toggleDiv(item);
+    if (!currentDiv && !currentBtn) {
+        closeDiv(item);
     }
 });
 
